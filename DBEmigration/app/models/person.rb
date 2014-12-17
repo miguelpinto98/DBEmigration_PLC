a8c9728fa6729b02ffb64fa3ac9aebeb1a9b071a
Bruno Ferreira <chalkos@chalkos.net>
2014-12-17 00:28:09 +0000

    já se pode definir estado civil e género com os atalhos do simple_enum

--- DBEmigration/app/models/person.rb
+++ DBEmigration/app/models/person.rb
@@ -9,12 +9,11 @@ class Person < ActiveRecord::Base
   has_one :localNasc, :foreign_key => :local_nasc
   has_one :localWork, :foreign_key => :local_work
 
-  # género: masculino ou feminino
+  # https://github.com/lwe/simple_enum
   as_enum :gender, :female => 1, :male => 0
-  # estado civil: solteiro, casado, divorciado, viúvo
   as_enum :civil, single: 0, married: 1, divorced: 3, widowed: 4
 
-  
-
+  attr_accessor :gender_cd
+  attr_accessor :civil_cd
 
 end
