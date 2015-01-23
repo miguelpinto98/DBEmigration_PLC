20a52d6ec88b97345a26289766bad5206a3dcb63
Miguel Pinto <mcpinto98@gmail.com>
2015-01-23 17:07:32 +0000

    all changes

--- DBEmigration/config/routes.rb
+++ DBEmigration/config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   resources :locals
   resources :photos
 
+
+get "/", to: "welcome#index", as: :home
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
