5a60fe4b36aca0d5adfc1a811791d97835d61041
Bruno Ferreira <chalkos@chalkos.net>
2015-01-25 22:48:59 +0000

    rails já chama o java e executa o ficheiro gerado pelo java

--- DBEmigration/app/controllers/passports_controller.rb
+++ DBEmigration/app/controllers/passports_controller.rb
@@ -1,3 +1,5 @@
+require 'popen4'
+
 class PassportsController < ApplicationController
   before_action :set_passport, only: [:show, :edit, :update, :destroy]
 
@@ -27,21 +29,71 @@ class PassportsController < ApplicationController
     @passport = Passport.new(passport_params)
 
     #obter o json
+    path = params[:passport]['p_file'].path
+
     #chamar o java com o json
+    java_out = ''
+    java_err = Array.new
+    status = POpen4::popen4( "java -jar lib/java/Passports.jar #{path}" ) do |stdout, stderr, stdin|
+      java_out = stdout.read
+      java_err = stderr.readlines
+        .select{|e|not e.start_with?('Picked up _JAVA_OPTIONS')}
+        .map{|e| e.chop! if e.end_with? "\n"}
+    end
+    puts "status: #{status.exitstatus}\n\nStdErr:\n#{java_err.to_s}\n\nStdOut.length:\n#{java_out.length}"
+
+    unless java_out.empty?
+      File.open("#{path}.rb", 'w') do |f|
+        f.write java_out
+      end
+      begin
+        load("#{path}.rb")
+      rescue Exception => e
+        puts "Exception ao correr o ficheiro ruby gerado pelo java"
+        puts "[message]\n#{e.message}"
+        print "[minimal-backtrace]\n" + e.backtrace.select{|e|e.include?('RackMultipart')}.join("\n")
+
+      end
+
+=begin
+      puts "rails r #{path}.rb"
+
+      status = POpen4::popen4( "rails r #{path}.rb" ) do |stdout, stderr, stdin|
+        ruby_out = stdout.read
+        ruby_err = stderr.read
+      end
+
+      puts status.exitstatus
+
+      puts 'ruby err'
+      puts ruby_err
+      puts 'ruby out'
+      puts ruby_out
+=end
+    else
+      #TODO: mostrar erros do java
+    end
+
+    
+
     #obter output + stderr
     #  se tem output, executar
     #    se deu erro, mostrar erro
     #  senao mostrar stderr, um erro por linha
 
     respond_to do |format|
-      if @passport.save
-        format.html { redirect_to pass_path, notice: 'Passport was successfully created.' }
-        format.json { render :show, status: :created, location: @passport }
-      else
-        format.html { render :new }
-        format.json { render json: @passport.errors, status: :unprocessable_entity }
-      end
+      format.html { render :text => "dummy"}
     end
+
+    #respond_to do |format|
+    #  if @passport.save
+    #    format.html { redirect_to pass_path, notice: 'Passport was successfully created.' }
+    #    format.json { render :show, status: :created, location: @passport }
+    #  else
+    #    format.html { render :new }
+    #    format.json { render json: @passport.errors, status: :unprocessable_entity }
+    #  end
+    #end
   end
 
   # PATCH/PUT /passports/1
